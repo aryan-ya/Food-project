e596c0576343e30a478293e67eb956f9ebaaf4fb
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-09 13:20:32 +0530

    food

---
+++ food-website/src/component/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+
+const Layout = () => {
+  return (
+    <div> 
+        <h1> Layout</h1>
+       
+    <Header />
+    </div>
+  )
+}
+
+export default Layout
